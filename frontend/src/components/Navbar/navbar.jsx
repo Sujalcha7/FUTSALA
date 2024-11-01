1e2999c2cd3068d8d092149ef788556904d823b8
Sujalcha7 <sujalkoju97@gmail.com>
2024-11-02 00:27:39 +0545

    frontend-in-working-state-but-not-properly-signup-not-working

---
+++ frontend/src/components/Navbar/navbar.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Box, Flex, Link as ChakraLink, Spacer } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
+
+function Navbar() {
+    return (
+        <Flex
+            as="nav"
+            align="center"
+            justify="space-between"
+            wrap="wrap"
+            padding="1.5rem"
+            bg="blue.500"
+            color="white"
+        >
+            <Flex align="center" mr={5}>
+                <ChakraLink
+                    as={RouterLink}
+                    to="/"
+                    fontWeight="bold"
+                    fontSize="lg"
+                >
+                    Reservation App
+                </ChakraLink>
+            </Flex>
+
+            <Box>
+                <ChakraLink as={RouterLink} to="/signup" mr={3}>
+                    Signup
+                </ChakraLink>
+                <ChakraLink as={RouterLink} to="/login" mr={3}>
+                    Login
+                </ChakraLink>
+                <ChakraLink as={RouterLink} to="/reservations" mr={3}>
+                    Reservations
+                </ChakraLink>
+                <ChakraLink as={RouterLink} to="/create-reservation">
+                    Create Reservation
+                </ChakraLink>
+            </Box>
+        </Flex>
+    );
+}
+
+export default Navbar;
