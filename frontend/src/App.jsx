9c7076e0a211ef7d59a70dddb74052438debcdcb
Subekshyeah <Kadelsubekshya@gmail.com>
2024-11-11 09:14:45 +0545

    contact us and about us

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,8 @@ import Signup from "./pages/Signup/signup";
 import Login from "./pages/Login/login";
 import Reservations from "./pages/Reservations/reservation";
 import CreateReservation from "./pages/CreateReservation/create_reservation";
+import AboutUsPage from "./pages/about_us/aboutus";
+import ContactUsPage from "./pages/contactus/contactus";
 
 function App() {
     return (
@@ -18,6 +20,8 @@ function App() {
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/reservations" element={<Reservations />} />
+                <Route path="/aboutus" element={<AboutUsPage />} />
+                <Route path="/contactus" element={<ContactUsPage />} />
                 <Route
                     path="/create-reservation"
                     element={<CreateReservation />}
