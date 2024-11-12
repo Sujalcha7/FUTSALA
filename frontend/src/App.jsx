fdaf148dced013f93e5c70d269fba1910cd368f0
Subekshyeah <Kadelsubekshya@gmail.com>
2024-11-12 08:56:44 +0545

    navbar edited with contact us and about us

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,8 @@ import Login from "./pages/Login/login";
 import Reservations from "./pages/Reservations/reservation";
 import CreateReservation from "./pages/CreateReservation/create_reservation";
 import Profile from "./pages/Profile/profile"; // Add this import
+import AboutUsPage from "./pages/about_us/aboutus";
+import ContactUsPage from "./pages/contactus/contactus";
 
 function App() {
     return (
@@ -22,6 +24,8 @@ function App() {
                     <Route path="/signup" element={<Signup />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/reservations" element={<Reservations />} />
+                    <Route path="/aboutus" element={<AboutUsPage />} />
+                    <Route path="/contactus" element={<ContactUsPage />} />
                     <Route
                         path="/create-reservation"
                         element={<CreateReservation />}
