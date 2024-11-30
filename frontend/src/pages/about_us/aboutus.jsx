e888543c5d4f1e628a861681cb8bdc42846ee2e6
Sreinumder <rojgosai02@gmail.com>
2024-11-30 11:03:25 +0545

    typo fix on aboutus and contactus

--- frontend/src/pages/about_us/aboutus.jsx
+++ frontend/src/pages/about_us/aboutus.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./AboutUs.css";
+import "./aboutus.css";
 import { Container } from "@chakra-ui/react";
 // import logo from './logo.png';
 
--- frontend/src/pages/contactus/contactus.jsx
+++ frontend/src/pages/contactus/contactus.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './ContactUs.css';
+import './contactus.css';
 // import logo from './logo.png';
 
 const ContactUsPage = () => {
