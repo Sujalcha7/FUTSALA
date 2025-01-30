7dfbfa6d6c846f0318cf096f3b7d05c70eb79a58
YoG-Anish <yogesh.anish01@gmail.com>
2025-01-30 14:45:19 +0545

    bunch of fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Box } from "@chakra-ui/react";
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Box, Center, Spinner } from "@chakra-ui/react";
+import { Routes, Route, useLocation, Navigate } from "react-router-dom"; // Import Navigate for redirection
+import { useAuth } from "./AuthContext"; // Import useAuth hook
 import Navbar from "./components/Navbar/navbar";
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/home";
@@ -22,89 +23,31 @@ import AssignTasks from "./pages/Employees/AssignTasks";
 import EditEmployee from "./pages/Employees/EditEmployee";
 import UsersList from "./pages/userList/userList";
 import UserReservations from "./pages/userReservations/userReservations";
-function App() {
-    // const { isLoading, user } = useAuth();
 
-    // if (isLoading) {
-    //     return (
-    //         <Center height="100vh">
-    //             <Spinner size="xl" />
-    //         </Center>
-    //     );
-    // }
-    // if (!user && !user?.role == "owner") {
-    //     return (
-    //         <Box>
-    //             <Navbar />
-    //             <Routes>
-    //                 <Route path="/" element={<Home />} />
-    //                 <Route path="/signup" element={<Signup />} />
-    //                 <Route path="/login" element={<Login />} />
-    //                 {/* <Route path="/reservations" element={<Reservations />} /> */}
-    //                 <Route path="/aboutus" element={<AboutUsPage />} />
-    //                 <Route path="/contactus" element={<ContactUsPage />} />
-    //                 <Route
-    //                     path="/create-reservation"
-    //                     element={<CreateReservation />}
-    //                 />
-    //                 <Route path="/profile" element={<Profile />} />
-    //                 <Route
-    //                     path="/superuser-dashboard"
-    //                     element={<SuperuserDashboard />}
-    //                 />
-    //             </Routes>
-    //             {/* <Footer /> */}
-    //         </Box>
-    //     );
-    // } else if (user && !user?.role == "owner") {
-    //     return (
-    //         <Box>
-    //             <Navbar />
-    //             <Routes>
-    //                 <Route path="/" element={<Home />} />
-    //                 <Route path="/signup" element={<Signup />} />
-    //                 <Route path="/login" element={<Login />} />
-    //                 <Route path="/aboutus" element={<AboutUsPage />} />
-    //                 <Route path="/contactus" element={<ContactUsPage />} />
-    //                 <Route
-    //                     path="/create-reservation"
-    //                     element={<CreateReservation />}
-    //                 />
-    //                 <Route path="/profile" element={<Profile />} />
-    //                 <Route
-    //                     path="/superuser-dashboard"
-    //                     element={<SuperuserDashboard />}
-    //                 />
-    //             </Routes>
-    //             <Footer />
-    //         </Box>
-    //     );
-    // } else if (user && user?.role == "owner") {
-    //     return (
-    //         <Box>
-    //             <Navbar />
-    //             <Routes>
-    //                 <Route path="/" element={<Home />} />
-    //                 <Route path="/signup" element={<Signup />} />
-    //                 <Route path="/login" element={<Login />} />
-    //                 <Route path="/reservations" element={<Reservations />} />
-    //                 <Route path="/aboutus" element={<AboutUsPage />} />
-    //                 <Route path="/contactus" element={<ContactUsPage />} />
-    //                 <Route
-    //                     path="/create-reservation"
-    //                     element={<CreateReservation />}
-    //                 />
-    //                 <Route path="/profile" element={<Profile />} />
-    //                 <Route
-    //                     path="/superuser-dashboard"
-    //                     element={<SuperuserDashboard />}
-    //                 />
-    //             </Routes>
-    //         </Box>
-    //     );
-    // }
+function App() {
+    const { user, isLoading } = useAuth(); // Access user data from AuthContext
+
+    // Show a loading spinner if the user data is still being fetched
+    if (isLoading) {
+        return (
+            <Center height="100vh">
+                <Spinner size="xl" />
+            </Center>
+        );
+    }
+
     const location = useLocation();
     const noFooterRoutes = ["/login", "/signup"];
+
+    // Redirect logged-in users from /login and /signup
+    if (user && (location.pathname === "/login" || location.pathname === "/signup")) {
+        return <Navigate to="/profile" replace />;
+    }
+
+    // Redirect logged-out users from other routes (except /login and /signup)
+    if (!user && location.pathname !== "/login" && location.pathname !== "/signup" && location.pathname !== "/") {
+        return <Navigate to="/login" replace />;
+    }
 
     return (
         <Box>
@@ -117,10 +60,7 @@ function App() {
                 <Route path="/employee-create" element={<EmployeeCreation />} />
                 <Route path="/assign-tasks" element={<AssignTasks />} />
                 <Route path="/edit-employee" element={<EditEmployee />} />
-                <Route
-                    path="/employee-tasks-list"
-                    element={<EmployeeTasksList />}
-                />
+                <Route path="/employee-tasks-list" element={<EmployeeTasksList />} />
                 <Route path="/reservations" element={<Reservations />} />
                 <Route path="/aboutus" element={<AboutUsPage />} />
                 <Route path="/contactus" element={<ContactUsPage />} />
@@ -128,11 +68,8 @@ function App() {
                 <Route path="/user-list" element={<UsersList />} />
                 <Route path="/court/:id" element={<CourtPage />} />
                 <Route path="/courts/" element={<Courts />} />
-                <Route path="/user-reservations" element={<UserReservations />} /> 
-                <Route
-                    path="/create-reservation/:id"
-                    element={<CreateReservation />}
-                />
+                <Route path="/user-reservations" element={<UserReservations />} />
+                <Route path="/create-reservation/:id" element={<CreateReservation />} />
                 <Route path="/profile" element={<Profile />} />
                 <Route path="/dashboard" element={<SuperuserDashboard />} />
             </Routes>
